39674df82ea9b5e7cb74e7dbc2bcd90883597a92
Sarthak Jain <successar@gmail.com>
2019-03-19 14:38:53 -0400

    Readmission

--- Experiments/evaluate.py
+++ Experiments/evaluate.py
@@ -5,7 +5,7 @@ from PatientVec.models.Vanilla import ClassificationTrainer as BasicCT
 from PatientVec.models.Hierarchical import ClassificationTrainer as HierCT
 from PatientVec.trainer import Trainer, Evaluator
 
-def get_evaluator(dataset, model_name, trainer='basic') :
-    model_dirname = get_latest_model(os.path.join('outputs', dataset.name, model_name))
+def get_evaluator(dataset, model_name, trainer='basic', basename='outputs') :
+    model_dirname = get_latest_model(os.path.join(basename, dataset.name, model_name))
     evaluator = Evaluator(BasicCT, model_dirname, _type=dataset.metrics_type, display_metrics=True)
     return evaluator
