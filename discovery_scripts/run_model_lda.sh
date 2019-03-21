d996068cf513f55989c87cea9df5ec6399d7dcdf
Sarthak Jain <successar@gmail.com>
2019-03-21 18:09:07 -0400

    Discovery LDA

--- discovery_scripts/run_model_lda.sh
+++ discovery_scripts/run_model_lda.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=run_model_lda                   
+#SBATCH --job-name=run_model_lda   
+#SBATCH --exclusive
 #SBATCH --nodes=1                                 
 #SBATCH --tasks-per-node=1                        
-#SBATCH --cpus-per-task=4                         
+#SBATCH --cpus-per-task=16                         
 #SBATCH --mem=40Gb                               
 #SBATCH --output=run_model_lda_result.%j.out               
 #SBATCH --error=run_model_lda_result.%j.err
